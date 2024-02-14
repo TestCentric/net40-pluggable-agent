0f7229e41a1503e8678e7cbed06495f316f1a30d
Charlie Poole <charliepoole@gmail.com>
2024-02-14 05:40:43 -0800

    Update dependencies

--- src/net40-agent/Net40Agent.cs
+++ src/net40-agent/Net40Agent.cs
@@ -3,14 +3,6 @@
 // Licensed under the MIT License. See LICENSE file in root directory.
 // ***********************************************************************
 
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Security;
-using TestCentric.Engine.Agents;
-using TestCentric.Engine.Internal;
-using TestCentric.Engine.Communication.Transports.Remoting;
-
 namespace TestCentric.Agents
 {
     public class Net462Agent : TestCentricAgent<Net462Agent>
